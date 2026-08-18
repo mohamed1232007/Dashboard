6746279b7feeeb7373bd5ef14a23144c131d2368
mohamed1232007 <mohamedibraim1232007@gmail.com>
2026-08-18 18:44:03 +0300

    c

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
 require("dotenv").config();
 const app = require("./app");
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-// تشغيل السيرفر محلياً فقط على جهازك
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-// تصدير Express عشان Vercel يعرف يقرأه
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
