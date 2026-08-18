b5829c997e37c523e3b3987d0de8d5da45a2e5d7
mohamed1232007 <mohamedibraim1232007@gmail.com>
2026-08-18 16:52:13 +0300

    up

--- app.js
+++ app.js
@@ -1,36 +1,31 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const methodOverride = require("method-override");
-const { connectMongo } = require("./config/db");
+const cookieParser = require("cookie-parser");
 
+const { connectMongo } = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const customerRoutes = require("./routes/customerRoutes");
 
 const app = express();
 
-// Set View Engine
+connectMongo();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.use(cookieParser());
+
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Middlewares
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());
-app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "frontend")));  
+app.use(express.static(path.join(__dirname, "public")));    
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "frontend")));
-
-// Middleware لضمان الاتصال بالمونجو في كل طلب على Vercel
-app.use(async (req, res, next) => {
-    await connectMongo();
-    next();
-});
-
-// Routes
-app.use("/api/users", authRoutes);
-app.use("/", customerRoutes);
+app.use("/api/users", authRoutes);  
+app.use("/", customerRoutes);       
 
 module.exports = app;
